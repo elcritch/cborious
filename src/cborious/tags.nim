99b6789e13909ce313ddb37e17f0889244941522
Jaremy Creechley <creechley@gmail.com>
2025-09-09 07:38:28 -0600

    updates

--- src/cborious/tags.nim
+++ src/cborious/tags.nim
@@ -16,10 +16,10 @@ proc cborTag*(tp: typedesc[DateTime]): CborTag =
 proc cborTag*(tp: typedesc[Time]): CborTag =
   result = CborTagEpochSeconds
 
-proc pack_tagged*[T](s: Stream, val: T) =
+proc pack_tagged_impl*[T](s: Stream, tag: CborTag, val: T) =
   ## Pack a value with a preceding tag.
-  s.pack_tag(cborTag(T))
-  s.pack_tagged(val)
+  s.pack_tag(tag)
+  s.pack_type(val)
 
 proc readOneTag*(s: Stream, tagOut: var CborTag): bool =
   ## Reads a single tag if present, returns true and sets tagOut. Restores position when not a tag.
@@ -31,13 +31,13 @@ proc readOneTag*(s: Stream, tagOut: var CborTag): bool =
   s.setPosition(pos)
   return false
 
-proc unpackExpectTag*[T](s: Stream, value: var T) =
+proc unpackExpectTag*[T](s: Stream, tag: CborTag, value: var T) =
   ## Requires the next item to be a tag with the specified id, then unpacks a value of type T.
   let (m, ai) = s.readInitial()
   if m != CborMajor.Tag:
     raise newException(CborInvalidHeaderError, "expected tag")
   let t = s.readAddInfo(ai)
-  if t != cborTag(T).uint64:
+  if t != tag.uint64:
     raise newException(CborInvalidHeaderError, "unexpected tag value")
   s.unpack_type(value)
 
@@ -59,21 +59,22 @@ proc unpack_tagged*(s: Stream, dt: var DateTime) =
   ## Decode tag(0) timestamp string into DateTime using provided format.
   ## If the format does not include an offset (no 'z'), the parsed DateTime is assigned assumeZone.
   var str: string
-  s.unpackExpectTag(str)
+  s.unpackExpectTag(CborTagDateTimeString, str)
+
   var dt: DateTime
-  if 'z' in fmt:
-    dt = parse(str, fmt)
-  elif "'Z'" in fmt:
-    let fmt2 = fmt.replace("'Z'", "zzz")
+  if 'z' in datetimeFmt:
+    dt = parse(str, datetimeFmt)
+  elif "'Z'" in datetimeFmt:
+    let fmt2 = datetimeFmt.replace("'Z'", "zzz")
     let str2 = str.replace("Z", "+00:00")
     dt = parse(str2, fmt2)
   else:
-    let tmp = parse(str, fmt)
+    let tmp = parse(str, datetimeFmt)
     dt = initDateTime(tmp.monthday, tmp.month, tmp.year, tmp.hour, tmp.minute, tmp.second, zone = assumeZone)
   dt = dt
 
 proc unpack_tagged*(s: Stream, t: var Time) =
   ## Decode tag(1) integer seconds into Time.
   var secs: int64
-  s.unpackExpectTag(secs)
+  s.unpackExpectTag(CborTagEpochSeconds, secs)
   t = fromUnix(secs)
--- tests/ttags.nim
+++ tests/ttags.nim
@@ -8,7 +8,7 @@ suite "CBOR tags & timestamps":
   test "tag 0: RFC3339 string roundtrip & generic skip":
     var dt = initDateTime(21, mMar, 2013, 20, 4, 0, zone = utc())
     var s = CborStream.init()
-    s.packTimestampString(dt)
+    s.pack_tagged(dt)
     # C0 (tag 0), then length-prefixed string
     check s.data.startsWith("\xc0\x74")
     s.setPosition(0)
@@ -19,7 +19,7 @@ suite "CBOR tags & timestamps":
   test "tag 1: epoch seconds example":
     var t = fromUnix(1363896240'i64)
     var s = CborStream.init()
-    s.packTimestamp(t)
+    s.pack_tagged(t)
     # C1 = tag 1, then 1A = uint32, followed by 0x514B67B0
     check s.data == "\xc1\x1a\x51\x4b\x67\xb0"
     s.setPosition(0)
@@ -29,8 +29,9 @@ suite "CBOR tags & timestamps":
 
   test "tag 0: decode into DateTime using format":
     var s = CborStream.init()
-    s.packTag(0.CborTag)
-    s.pack("2013-03-21T20:04:00Z")
+    s.pack_tagged_impl(0.CborTag, "2013-03-21T20:04:00Z")
     s.setPosition(0)
-    let dt = cbortimes.unpackTimestampString(s, "yyyy-MM-dd'T'HH:mm:ss'Z'")
+
+    var dt: DateTime
+    unpack_tagged(s, dt)
     check dt == initDateTime(21, mMar, 2013, 20, 4, 0, zone = utc())
