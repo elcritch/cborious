c0e6b6e96bb0b6d17dcb351b9070149dc665cbc0
Jaremy Creechley <creechley@gmail.com>
2025-09-09 21:46:07 -0600

    updates

--- tests/ttags.nim
+++ tests/ttags.nim
@@ -20,7 +20,7 @@ suite "CBOR tags & timestamps":
     var s = CborStream.init()
     s.cborPack("2013-03-21T20:04:00Z")
     # C0 (tag 0), then length-prefixed string
-    checkPackToString(s, "\xC0\x642013-03-21T20:04:00Z")
+    checkPackToString(s, "\xC0\x742013-03-21T20:04:00Z")
     # Unpack as string should ignore tag
     s.setPosition(0)
     let txt = unpack(s, string)
