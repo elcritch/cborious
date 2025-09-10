9bdd721624a415d7e701f762b6175726dbb5ce8c
Jaremy Creechley <creechley@gmail.com>
2025-09-09 21:45:45 -0600

    updates

--- tests/ttags.nim
+++ tests/ttags.nim
@@ -10,11 +10,17 @@ template checkPackToString(v: CborStream, expected: string) =
   check v.data.repr() == expected.repr()
 
 suite "CBOR tags & timestamps":
+  test "test cborPackTag":
+    var cc = CborStream.init()
+    cc.cborPackTag(cborTag(DateTime))
+    cc.cborPack("2013-03-21T20:04:00Z")
+    checkPackToString(cc, "\192\x742013-03-21T20:04:00Z")
+
   test "tag 0: string roundtrip & generic skip":
     var s = CborStream.init()
     s.cborPack("2013-03-21T20:04:00Z")
     # C0 (tag 0), then length-prefixed string
-    # checkPackToString(s, "\x642013-03-21T20:04:00Z")
+    checkPackToString(s, "\xC0\x642013-03-21T20:04:00Z")
     # Unpack as string should ignore tag
     s.setPosition(0)
     let txt = unpack(s, string)
