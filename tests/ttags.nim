cbb9563bb1e4c66dd6f8e797a8c461e7d7311a6b
Jaremy Creechley <creechley@gmail.com>
2025-09-09 22:08:26 -0600

    updates

--- tests/ttags.nim
+++ tests/ttags.nim
@@ -3,23 +3,20 @@ import std/times
 import std/strutils
 import cborious
 
-template checkPackToString(v: CborStream, expected: string) =
-  echo "checking " & $v.data.repr() & " (" & $typeof(v.data) & ")" & " to " & expected.repr()
-  check v.data.repr() == expected.repr()
 
 suite "CBOR tags & timestamps":
   test "test cborPackTag":
     var cc = CborStream.init()
     cc.cborPackTag(cborTag(DateTime))
     cc.cborPack("2013-03-21T20:04:00Z")
-    checkPackToString(cc, "\192\x742013-03-21T20:04:00Z")
+    check cc.data.repr() == repr("\192\x742013-03-21T20:04:00Z")
 
   test "tag 0: string roundtrip & generic skip":
     var s = CborStream.init()
-    let dt = parse("2013-03-21T20:04:00Z", "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    var dt: DateTime = parse("2013-03-21T20:04:00Z", "yyyy-MM-dd'T'HH:mm:ss'Z'")
     s.cborPack(dt)
     # C0 (tag 0), then length-prefixed string
-    checkPackToString(s, "\xC0\x742013-03-21T20:04:00Z")
+    check s.data.repr() == repr("\xC0\x742013-03-21T20:04:00Z")
     # Unpack as string should ignore tag
     s.setPosition(0)
     let txt = unpack(s, DateTime)
